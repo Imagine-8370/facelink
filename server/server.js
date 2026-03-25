73f1a820237609cdf62286b3b0561525d4f8eefb
Advait <advaitchithira@gmail.com>
2026-03-25 15:20:23 +0530

    Add user matching and socket.io integration
    Implemented user matching functionality with socket.io for real-time communication.

--- server/server.js
+++ server/server.js
@@ -9,6 +9,56 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "../client")));
 
 const server = http.createServer(app);
+const io = new Server(server);
+
+let queue = [];
+
+function matchUsers() {
+  while (queue.length >= 2) {
+    const a = queue.shift();
+    const b = queue.shift();
+
+    a.partner = b;
+    b.partner = a;
+
+    a.emit("matched", { initiator: true });
+    b.emit("matched", { initiator: false });
+  }
+}
+
+io.on("connection", (socket) => {
+  console.log("User:", socket.id);
+
+  socket.on("join", () => {
+    queue.push(socket);
+    matchUsers();
+  });
+
+  socket.on("offer", d => socket.partner?.emit("offer", d));
+  socket.on("answer", d => socket.partner?.emit("answer", d));
+  socket.on("ice-candidate", d => socket.partner?.emit("ice-candidate", d));
+
+  socket.on("disconnect", () => {
+    queue = queue.filter(s => s !== socket);
+
+    if (socket.partner) {
+      socket.partner.partner = null;
+      socket.partner.emit("partner-disconnected");
+    }
+  });
+});
+
+server.listen(process.env.PORT || 3000);const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const path = require("path");
+const { Server } = require("socket.io");
+
+const app = express();
+app.use(cors());
+app.use(express.static(path.join(__dirname, "../client")));
+
+const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
