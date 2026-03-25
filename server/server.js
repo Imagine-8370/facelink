3c508bac955eceecfd9bfe221098d4db1aa9184a
Advait <advaitchithira@gmail.com>
2026-03-25 12:48:49 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,58 @@
-require("dotenv").config();
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const path = require("path");
+const { Server } = require("socket.io");
+
+const app = express();
+app.use(cors());
+app.use(express.static(path.join(__dirname, "../client")));
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+let queue = new Set();
+
+function matchUsers() {
+  const users = Array.from(queue);
+
+  while (users.length >= 2) {
+    const a = users.shift();
+    const b = users.shift();
+
+    queue.delete(a);
+    queue.delete(b);
+
+    a.partner = b;
+    b.partner = a;
+
+    a.emit("matched", { initiator: true });
+    b.emit("matched", { initiator: false });
+  }
+}
+
+io.on("connection", (socket) => {
+  socket.on("join", () => {
+    queue.add(socket);
+    matchUsers();
+  });
+
+  socket.on("offer", d => socket.partner?.emit("offer", d));
+  socket.on("answer", d => socket.partner?.emit("answer", d));
+  socket.on("ice-candidate", d => socket.partner?.emit("ice-candidate", d));
+
+  socket.on("disconnect", () => {
+    queue.delete(socket);
+
+    if (socket.partner) {
+      socket.partner.partner = null;
+      socket.partner.emit("partner-disconnected");
+    }
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log("Facelink running 🚀"));require("dotenv").config();
 
 const express = require("express");
 const http = require("http");
